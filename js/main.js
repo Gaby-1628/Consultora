0d09ec7e5de51c9c0e87dc457493f8f6b6447507
Marisol Rodriguez <wvk_28@hotmail.com>
2023-03-11 13:29:08 -0300

    Se acegro archivo js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+    
+    sr.reveal('#mision', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('#vision-texto', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('#valores-texto', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    });
