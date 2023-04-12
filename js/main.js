6e9caf146986515b98fa129093c7be57983b0fea
NahuelSchmidt <nahuelschmidt@MacBook-Air-de-Nahuel.local>
2023-04-12 18:07:27 -0300

    se modifico la seccion "testimonios"

--- js/main.js
+++ js/main.js
@@ -94,42 +94,6 @@ sr.reveal('#banner h1', {
 
 /* --------------------- TESTIMONIOS ------------------ */ 
 
- /*const carousel = document.querySelector(".carousel");
-firstImg = carousel.querySelectorAll("img")[0];
-arrowIcons  = document.querySelectorAll(".wrapper i");
-
-let isDragStart = false, prevPageX, prevScrollLeft;
-let firstImgWidth = firstImg.clientWidth + 14;
-
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click ", () => {
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-    });
-});
-
-const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
-
-const dragging = (e) => {
-    if(!isDragStart) return;
-    e.preventDefault();
-    let positionDiff = e.pageX - prevPageX
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
-
-const dragStop = () => {
-    isDragStart = false;
-}
-
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("mouseup", dragStop);
- */
-
 
 const carouselImages = document.querySelector(".carousel-images");
 const carouselPrevButton = document.querySelector(".carousel-button-prev");
@@ -150,4 +114,18 @@ carouselNextButton.addEventListener("click", () => {
     currentIndex = 0;
   }
   carouselImages.style.transform = `translateX(-${currentIndex * 33.33}%)`;
+});
+
+
+
+sr.reveal('#testi- p', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('carousel', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
 });
