2b654d7f3c8fdc251731dd61d372310127033f96
Marisol Rodriguez <wvk_28@hotmail.com>
2023-05-28 21:59:24 -0300

    Se modifico la seccion Testimonios

---
+++ js/nav_vertical.js
@@ -0,0 +1,25 @@
+const radanet = document.querySelector('#radanet')
+const epl = document.querySelector('#epl')
+const mentes = document.querySelector('#mentes')
+
+epl.style.display = 'none'
+mentes.style.display = 'none'
+
+function showHide(escenarios)
+{
+    if( escenarios == 'epl'){
+        radanet.style.display = 'none'
+        epl.style.display = 'flex'
+        mentes.style.display = 'none'
+    }
+    else if( escenarios == 'mentes' ){
+        radanet.style.display = 'none'
+        epl.style.display = 'none'
+        mentes.style.display = 'flex'
+    }
+    else if( escenarios == 'radanet' ){
+        radanet.style.display = 'flex'
+        epl.style.display = 'none'
+        mentes.style.display = 'none'
+    }
+}
